d5d9455a1b1655d72aa26ce70964d616dfd2f938
newDINO <new_dino@126.com>
2024-10-29 10:46:50 +0800

    speed up tests

--- tests/read_write.rs
+++ tests/read_write.rs
@@ -1,14 +1,16 @@
+use futures_lite::{AsyncReadExt, AsyncWriteExt};
 use wasm_bindgen_test::*;
 
 use wasm_bindgen_test::wasm_bindgen_test_configure;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+use web_fs::*;
+
 #[wasm_bindgen_test]
-async fn test() {
+async fn read_write() {
     use futures_lite::AsyncWriteExt;
     use futures_lite::AsyncReadExt;
-    use web_fs::{File, read_to_string, write, OpenOptions, copy};
     // write
     {
         let mut file = File::create("testf").await.unwrap();
@@ -48,5 +50,34 @@ async fn test() {
         let mut buf = String::new();
         file.read_to_string(&mut buf).await.unwrap();
         assert_eq!("Hello", buf);
+    }
+}
+
+#[wasm_bindgen_test]
+async fn multi_file() {
+    create_dir("dir1").await.unwrap();
+    create_dir_all("dir2/dir1").await.unwrap();
+
+    {
+        let mut file1 = File::create("dir1/file1").await.unwrap();
+        let mut file2 = File::create("dir2/dir1/file1").await.unwrap();
+        file1.write_all(b"This is file1").await.unwrap();
+        file2.write_all(b"This is file2").await.unwrap();
+    }
+    {
+        let mut file1 = File::open("dir1/file1").await.unwrap();
+        let mut file2 = File::open("dir2/dir1/file1").await.unwrap();
+        let mut file3 = File::create("dir2/file1").await.unwrap();
+        file3.write_all(b"This is file3").await.unwrap();
+        let mut buf1 = String::new();
+        let mut buf2 = String::new();
+        file1.read_to_string(&mut buf1).await.unwrap();
+        file2.read_to_string(&mut buf2).await.unwrap();
+        assert_eq!("This is file1", buf1);
+        assert_eq!("This is file2", buf2);
+    }
+    {
+        let buf = read_to_string("dir2/file1").await.unwrap();
+        assert_eq!("This is file3", buf);
     }
 }
